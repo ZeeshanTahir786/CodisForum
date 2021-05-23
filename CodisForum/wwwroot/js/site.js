73bd8602ee7568c3871bb8761a45b182424864ee
Hafiz ZeeShan <hafizzeeshan138@gmail.com>
2021-05-23 16:05:56 +0500

    Initial commit

---
+++ CodisForum/wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("#signup").click(function () {
+    $("#first").fadeOut("fast", function () {
+        $("#second").fadeIn("fast");
+    });
+});
+
+$(function () {
+    $("form[name='login']").validate({
+        rules: {
+
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+
+            }
+        },
+        messages: {
+            email: "Please enter a valid email address",
+
+            password: {
+                required: "Please enter password",
+
+            }
+
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
+
